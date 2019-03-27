3e92208e6f1f06a2975e7b1bd02dca7c02ac97b9
Michel Hidalgo <michel@ekumenlabs.com>
2019-03-27 11:05:32 -0300

    Rename launchtest to launch_test.
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- launch_testing/test/launch_testing/test_xml_output.py
+++ launch_testing/test/launch_testing/test_xml_output.py
@@ -42,7 +42,7 @@ class TestGoodXmlOutput(unittest.TestCase):
 
         assert 0 == subprocess.run(
             args=[
-                'launchtest',
+                'launch_test',
                 path,
                 '--junit-xml', os.path.join(cls.tmpdir.name, 'junit.xml'),
             ],
